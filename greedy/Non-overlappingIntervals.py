95aac6adfeb71905435f6827f097b7cc16547bb0
mishapeshko <101748615+mishapeshko@users.noreply.github.com>
2026-09-23 18:44:08 +0200

    Update Non-overlappingIntervals.py

--- greedy/Non-overlappingIntervals.py
+++ greedy/Non-overlappingIntervals.py
@@ -2,17 +2,14 @@ class Solution(object):
     def eraseOverlapIntervals(self, intervals):
         intervals = sorted(intervals)
         res = 0
-        new = []
-        new.append(intervals[0])
-        act = 0
+        last = 0
         for i in range(1, len(intervals)):
-            if intervals[i][0] < new[act][1]:
+            if intervals[i][0] < intervals[last][1]:
                 res += 1
-                if intervals[i][1] >= new[act][1]:
+                if intervals[i][1] >= intervals[last][1]:
                     continue
                 else:
-                    new[act] = intervals[i]
+                    last = i
             else:
-                new.append(intervals[i])
-                act += 1
+                last = i
         return res
